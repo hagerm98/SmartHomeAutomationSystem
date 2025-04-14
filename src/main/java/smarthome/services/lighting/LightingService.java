8122e19d57303c966131374adcc945212168815e
Hager Khamis <hagerm98@hotmail.com>
2025-04-14 22:24:31 +0100

    Implemented Lighting service unary operations

--- src/main/java/smarthome/services/lighting/LightingService.java
+++ src/main/java/smarthome/services/lighting/LightingService.java
@@ -4,14 +4,34 @@ import io.grpc.stub.StreamObserver;
 import smarthome.generated.lighting.*;
 import smarthome.generated.lighting.LightingServiceGrpc.LightingServiceImplBase;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class LightingService extends LightingServiceImplBase {
+
+    Map<Integer, LightingDeviceDetails> lightingDetails = new HashMap<>();
 
     @Override
     public void setLightingState(
             LightingStateRequest request,
             StreamObserver<LightingDeviceDetails> responseObserver
     ) {
-        // TODO
+        if (!lightingDetails.containsKey(request.getDeviceNumber())) {
+            responseObserver.onError(new Exception("No lighting device with the given number '"
+                    + request.getDeviceNumber() + "' exists."));
+        } else {
+            LightingDeviceDetails oldLightingDeviceDetails = lightingDetails.get(request.getDeviceNumber());
+
+            LightingDeviceDetails lightingDeviceDetails = LightingDeviceDetails.newBuilder(oldLightingDeviceDetails)
+                    .setLightingDeviceState(request.getLightingState())
+                    .build();
+
+            lightingDetails.put(request.getDeviceNumber(), lightingDeviceDetails);
+
+            responseObserver.onNext(lightingDeviceDetails);
+        }
+
+        responseObserver.onCompleted();
     }
 
     @Override
@@ -19,7 +39,22 @@ public class LightingService extends LightingServiceImplBase {
             LightingBrightnessRequest request,
             StreamObserver<LightingDeviceDetails> responseObserver
     ) {
-        // TODO
+        if (!lightingDetails.containsKey(request.getDeviceNumber())) {
+            responseObserver.onError(new Exception("No lighting device with the given number '"
+                    + request.getDeviceNumber() + "' exists."));
+        } else {
+            LightingDeviceDetails oldLightingDeviceDetails = lightingDetails.get(request.getDeviceNumber());
+
+            LightingDeviceDetails lightingDeviceDetails = LightingDeviceDetails.newBuilder(oldLightingDeviceDetails)
+                    .setBrightness(request.getBrightness())
+                    .build();
+
+            lightingDetails.put(request.getDeviceNumber(), lightingDeviceDetails);
+
+            responseObserver.onNext(lightingDeviceDetails);
+        }
+
+        responseObserver.onCompleted();
     }
 
     @Override
@@ -35,7 +70,23 @@ public class LightingService extends LightingServiceImplBase {
             LightingDevice request,
             StreamObserver<LightingDeviceDetails> responseObserver
     ) {
-        // TODO
+
+        if (lightingDetails.containsKey(request.getDeviceNumber())) {
+            responseObserver.onError(new Exception("There's a lighting device with the same number '"
+                    + request.getDeviceNumber() + "' already exists."));
+        } else {
+            LightingDeviceDetails lightingDeviceDetails = LightingDeviceDetails.newBuilder()
+                    .setLightingDevice(request)
+                    .setLightingDeviceState(DeviceState.OFF)
+                    .setBrightness(0.0f)
+                    .build();
+
+            lightingDetails.put(request.getDeviceNumber(), lightingDeviceDetails);
+
+            responseObserver.onNext(lightingDeviceDetails);
+        }
+
+        responseObserver.onCompleted();
     }
 
     @Override
@@ -43,7 +94,16 @@ public class LightingService extends LightingServiceImplBase {
             LightingDevice request,
             StreamObserver<LightingDeviceDetails> responseObserver
     ) {
-        // TODO
+        if (!lightingDetails.containsKey(request.getDeviceNumber())) {
+            responseObserver.onError(new Exception("No lighting device with the given number '"
+                    + request.getDeviceNumber() + "' exists."));
+        } else {
+            LightingDeviceDetails lightingDeviceDetails = lightingDetails.remove(request.getDeviceNumber());
+
+            responseObserver.onNext(lightingDeviceDetails);
+        }
+
+        responseObserver.onCompleted();
     }
 
     @Override
